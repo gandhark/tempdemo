89766a7a132ac15627f7a05e6da34bcce1b7572b
gandhark <gandhark@users.noreply.github.com>
2016-06-15 17:00:33 +0530

    not done

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -1,6 +1,6 @@
 <?php
 if(!class_exists('ecomm_project/Config')): require('ecomm_project/Config '); endif;
-//require_once("ecomm_project/Helper.php");
+require_once("ecomm_project/Helper.php");
 //include("Product.php");
 
 //$helper=new Helper();
