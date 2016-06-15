bfbed1efb3cd215d76296455f5b901df40ba1f73
user6 <gandhark@cybage.com>
2016-06-15 15:14:44 +0530

    qe

---
+++ tests/HelperTest.php
@@ -0,0 +1,42 @@
+<?php
+
+include("Helper.php");
+//include("Product.php");
+
+//$helper=new Helper();
+class HelperTest extends PHPUnit_Framework_TestCase
+	{
+		public $helper;
+		public function testinsert()
+		{
+			$this->helper=new Helper("ecomm");
+			print_r( $this->helper->insert("a","a","a","a")	);
+			//$this->asser('172.27.59.54', true);
+			$this->assertEquals("1",$this->helper->insert("a","a","a","a"));
+
+		}
+	/*	public function testread_record()
+		{
+			$this->helper=new Helper("ecomm");
+                        //$this->asser('172.27.59.54', true);
+                        $this->assertEquals("should be Array","array",gettype($this->helper->read_record()));
+		}
+/*	public function testupdateProduct()
+		{
+			$this->assertTrue(true);
+		}
+		public function testdeleteProduct()
+		{
+//			$this->assertEquals('ecomm@123', Config::PASSWORD);
+			$this->assertTrue(true);
+		}
+		public function testviewProduct()
+		{
+			//$this->assertEquals('Heler.php', Config::HELPER);
+			$this->assertTrue(false);
+			 $this->assertEquals(['a', 'b', 'c'], ['a', 'c', 'd']);
+		}
+*/
+
+	}
+	
